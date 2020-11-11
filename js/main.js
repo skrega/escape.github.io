4a3dc72afe9bc84773daa2913a391166df0acbc0
skrega <skorpionn440@gmail.com>
2020-11-11 20:11:32 +1000

    added map

--- js/main.js
+++ js/main.js
@@ -161,5 +161,164 @@ $(function(){
           },
       ]
     });
-    
-})
+
+});
+
+    let map;
+
+    function initMap() {
+      map = new google.maps.Map(document.getElementById("map"), {
+          
+        center: { lat: 43.1502151, lng: 131.906339 },
+        zoom: 17,
+        disableDefaultUI: false,
+        scaleControl: false,
+        zoomControl: false,
+        fullscreenControl: false,
+        mapTypeControl: false,
+        streetViewControl: false,
+        
+        styles: [{
+            "featureType": "landscape.natural",
+            "stylers": [{
+                "color": "#bcddff"
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#5fb3ff"
+            }]
+        }, {
+            "featureType": "road.arterial",
+            "stylers": [{
+                "color": "#ebf4ff"
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#ebf4ff"
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                    "visibility": "on"
+                },
+                {
+                    "color": "#93c8ff"
+                }
+            ]
+        }, {
+            "featureType": "landscape.man_made",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#c7e2ff"
+            }]
+        }, {
+            "featureType": "transit.station.airport",
+            "elementType": "geometry",
+            "stylers": [{
+                    "saturation": 100
+                },
+                {
+                    "gamma": 0.82
+                },
+                {
+                    "hue": "#0088ff"
+                }
+            ]
+        }, {
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#1673cb"
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "labels.icon",
+            "stylers": [{
+                    "saturation": 58
+                },
+                {
+                    "hue": "#006eff"
+                }
+            ]
+        }, {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#4797e0"
+            }]
+        }, {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [{
+                    "color": "#209ee1"
+                },
+                {
+                    "lightness": 49
+                }
+            ]
+        }, {
+            "featureType": "transit.line",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#83befc"
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "color": "#3ea3ff"
+            }]
+        }, {
+            "featureType": "administrative",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                    "saturation": 86
+                },
+                {
+                    "hue": "#0077ff"
+                },
+                {
+                    "weight": 0.8
+                }
+            ]
+        }, {
+            "elementType": "labels.icon",
+            "stylers": [{
+                    "hue": "#0066ff"
+                },
+                {
+                    "weight": 1.9
+                }
+            ]
+        }, {
+            "featureType": "poi",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                    "hue": "#0077ff"
+                },
+                {
+                    "saturation": -7
+                },
+                {
+                    "lightness": 24
+                }
+            ]
+        }]
+        
+      }); 
+      new google.maps.Marker({
+        position: myLatLng,
+        map,
+        title: "Escape",
+      });
+      const marker = new google.maps.Marker({
+        position: {lat: 43.1502151, lng: 131.906339},
+        map: map,
+      });
+      
+      
+    }
